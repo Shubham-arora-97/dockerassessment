6ce50d2509ce179ee4b0822f08a0baec170be559
Shubham-arora-97 <52985438+Shubham-arora-97@users.noreply.github.com>
2019-08-19 17:54:19 +0530

    Add files via upload

---
+++ dockerassessment/src/main/java/com/training/dockerassessment/DockerassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.training.dockerassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerassessmentApplication.class, args);
+	}
+
+}
